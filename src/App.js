907b28324e3a29e21a5f2f69b4f49f8e4bdc97a0
kishore <kishore.sai37@gmail.com>
2022-03-16 13:26:11 +0530

    added starter files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-const App = () => {
-  return (
-    <>
-      <header class='header'>
-        <h1>Hello Everyone</h1>
-      </header>
-    </>
-  );
-};
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
